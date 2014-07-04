b973d135b9b0272c1a83a9ed1183577328994fd2
Scott Evans <git@scott.ee>
2014-07-04 12:07:16 +0100

    Add filter for search query 'wpasw_query'

--- wp-ajax-search-widget.php
+++ wp-ajax-search-widget.php
@@ -170,7 +170,7 @@ class wpasw_widget extends WP_Widget {
 			// set the query limit
 			$limit = empty($instance['number']) ? 10: $instance['number'];
 
-			$query_args = array('s' => $s, 'post_status' => 'publish', 'posts_per_page' => $limit );
+			$query_args = apply_filters( 'wpasw_query', array('s' => $s, 'post_status' => 'publish', 'posts_per_page' => $limit ) );
 
 			$search = new WP_Query($query_args);
 
@@ -190,6 +190,9 @@ class wpasw_widget extends WP_Widget {
 						<?php
 					}
 				endwhile;
+
+				// link to more?
+
 				?></ul><?php
 
 			else:
